66ec28d2aac10c421aa089e7c11af48e30fabeb5
davidcasstro <00369425@uca.edu.sv>
2026-02-14 10:31:40 -0600

    Funcion insertar

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 // Estructura de nodo:
 struct Nodo
@@ -7,6 +8,14 @@ struct Nodo
     float puntaje;
     char id;
     Nodo *siguiente;
+
+    Nodo(string nombre, char id){
+        this->nombre = nombre;
+        this->id= id;
+        puntaje = 0;
+        siguiente = nullptr;
+
+    }
 };
 
 
@@ -26,6 +35,26 @@ class ListaCircular{
     Nodo*obtenerSiguiente(Nodo* nodo);
     Nodo*obtenerGanador();
 };
+
+
+void ListaCircular::insertar(string nombre, char id){
+    Nodo* nuevo = new Nodo(nombre, id);
+
+    if (estaVacia()){
+        cabeza = nuevo;
+        nuevo -> siguiente = cabeza;
+    } else {
+        Nodo* temp= cabeza;
+        while(temp->siguiente!=cabeza){
+            temp = temp-> siguiente;
+        }
+
+        temp -> siguiente =nuevo;
+        nuevo -> siguiente = cabeza;
+    }
+
+
+}
 int main(){
 
 
