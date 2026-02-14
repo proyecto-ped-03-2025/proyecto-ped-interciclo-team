56e12395dcfcdfb06646fa777e156277b3a64970
davidcasstro <00369425@uca.edu.sv>
2026-02-13 22:03:11 -0600

    Implementar clase Lista circular

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 
+
+class ListaCircular{
+    private:
+    Nodo* cabeza;
+    
+    public:
+    ListaCircular();
+    void insertar(string nombre, char id);
+    void mostrar();
+    bool estaVacia();
+    int obtenerTamano();
+    Nodo*obtenerCabeza();
+    void reiniciarPuntajes();
+    Nodo*obtenerSiguiente(Nodo* nodo);
+    Nodo*obtenerGanador();
+};
 int main(){
 
 
